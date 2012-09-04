6e7367c5b3b3bd7de4e8ac682369d951bbbc918d
David Roerich <david.roerich@gmail.com>
2012-09-04 23:26:10 +0200

    Soundcloud-API-Quelldateien zum Paket hinzugefügt. In der Soundcloud.php musste noch eine Änderung vorgenommen werden, damit die Syntax kompatibel zu PHP 5.2 ist: in der Funktion updatePlaylist() wurde bisher eine Inline-Funktion verwendet. Diese ist nun in Form von track_string_callback() ausgelagert.
    Bei Soundcloud kann man offensichtlich keine Redirect-URIs angeben, die ein '&'
    enthalten. Deswegen funtkioniert der Ansatz nicht, einen HTTP-Parameter in der
    Redirect-URI zu übergeben. Dies soll mit dem Parameter 'state' gehen.
    Vielleicht geht es auch einfacher mit Abfrage von 'code'.

--- org.freakshit.useroption.soundcloudconnect/lib/acp/option/OptionTypeLinkerimage.class.php
+++ org.freakshit.useroption.soundcloudconnect/lib/acp/option/OptionTypeLinkerimage.class.php
@@ -1,15 +1,15 @@
 <?php
 // wcf imports
 require_once(WCF_DIR.'lib/acp/option/OptionType.class.php');
-require_once(WCF_DIR.'lib/data/soundcloud/SoundcloudConnect.class.php');
 
 // Souncloud API
-require_once 'Services/Soundcloud.php';
+require_once (WCF_DIR . 'lib/data/soundcloud/Soundcloud.php');
 
 // Define constants for Souncloud API registration
 define('CLIENT_ID', 'd3b9d13d8379b6c19044e4ec6a321102');
 define('CLIENT_SECRET', '9c3b63959977bf61404110fc603427f2');
-define('REDIRECT_URL', 'http://www.meniunddeve.de/sclapi/callback.php');
+define('REDIRECT_URL', 'http://www.meniunddeve.de/wbb-test/index.php?form=UserProfileEdit');
+//define('REDIRECT_URL', 'http://www.meniunddeve.de/wbb-test/scredirect.php');
 
 /**
  * My implementation of a user option type
@@ -30,7 +30,7 @@ class OptionTypeLinkerimage implements OptionType{
 		$buttonImageUrl = 'http://connect.soundcloud.com/2/btn-connect-l.png';
 		
 		// Get SC authorize URL
-		$sc_authorizeUrl = $sc_client->getAuthorizeUrl();
+		$sc_authorizeUrl = $sc_client->getAuthorizeUrl() . '&state=soundcloudConnect';
 		
 		$return = 'Übrigens ist der Wert von WCF_DIR ' . WCF_DIR . '</br>';
 		$return .= "<a href=\"$sc_authorizeUrl\" target=\"_blank\">";
@@ -59,9 +59,6 @@ class OptionTypeLinkerimage implements OptionType{
 		} else {
 			$return .= 'Habe keine action empfangen.</br>';
 		}
-		
-		
-		
 		
 		return $return;
 	}
