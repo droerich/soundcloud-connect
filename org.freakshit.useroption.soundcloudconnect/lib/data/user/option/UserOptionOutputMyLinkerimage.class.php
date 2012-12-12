7004f68f841177321128cc04661b211a156132d5
David Roerich <david.roerich@gmail.com>
2012-12-12 17:01:48 +0100

    Funktion isUserConnected() in der Klasse UserOptionOutputMyLinkerimage entfernt, da sie nicht benötigt wird.

--- org.freakshit.useroption.soundcloudconnect/lib/data/user/option/UserOptionOutputMyLinkerimage.class.php
+++ org.freakshit.useroption.soundcloudconnect/lib/data/user/option/UserOptionOutputMyLinkerimage.class.php
@@ -31,7 +31,7 @@ class UserOptionOutputMyLinkerimage implements UserOptionOutput {
 	
 		$return = '';
 		$sc_userId = $this->fetchSoundcloudId( $user->userID );
-		if ( !empty($sc_userId) ){			
+		if ( !empty($sc_userId) ){
 			$return .= '<iframe width="100%" height="450" scrolling="no" frameborder="no" ';
 			$return .= 'src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Fusers%2F' . $sc_userId;
 			$return .= '&show_artwork=true"></iframe>';
@@ -39,16 +39,6 @@ class UserOptionOutputMyLinkerimage implements UserOptionOutput {
 			$return .= 'Der Benutzer hat noch kein Soundcloud-Profil angegeben.';
 		}
 		return $return;
-	}
-	
-	private function isUserConnected( $wcf_userID ) {
-		$sqlQuery = 'SELECT soundcloudID
-			     FROM wcf' . WCF_N . '_user_soundcloud_connect
-			     WHERE userID = ' . intval($wcf_userID);
-		$row = WCF::getDB()->getFirstRow( $sqlQuery );
-		
-		$sc_userId = $row['soundcloudID'];
-		return !empty($sc_userId);
 	}
 	
 	private function fetchSoundcloudId( $wcf_userID ){
